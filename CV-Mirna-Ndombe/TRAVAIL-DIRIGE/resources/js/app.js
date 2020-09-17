d1e29d64f48c90a94c0dc85bc0cdb4d0131e0ef9
mirna36 <69789618+mirna36@users.noreply.github.com>
2020-09-17 15:15:52 +0200

    v6

--- CV-Mirna-Ndombe/TRAVAIL-DIRIGE/resources/js/app.js
+++ CV-Mirna-Ndombe/TRAVAIL-DIRIGE/resources/js/app.js
@@ -1,61 +1,83 @@
-//Initialisation du jeu
+// 1-Initialisation du jeu
 
 let scoresGlobals = [0, 0];
 let scoreEnCours = 0;
 let joueurActif = 1;
+let jouer = true;
 
-initialiseLejeu()
+//2-
+initialiseLejeu();
 
-let btnLancezLeDe = document.querySelector('.btn-lancer');
+//3
+let btnLancezLeDe = document.querySelector(`.btn-lancer`);
 btnLancezLeDe.addEventListener('click', () => {
-    function getRandomint() {
-        return Math.floor(Math.random() * 6) + 1;
-    }
-
-   let de=getRandomint();
+    if (jouer) {
+        let de = Math.floor(Math.random() * 6) + 1;
 
     let domDe = document.querySelector('.de');
     domDe.style.display = 'block';
     domDe.src = `resources/images/de-${de}.png`;
 
 
-if (de !== 1) {
-    scoreEnCours += de;
-    document.getElementById(`encours-${joueurActif}`).textContent = scoreEnCours;
-} else {
-
+    if (de !== 1) {
+        scoreEnCours += de;
+        document.getElementById(`encours-${joueurActif}`).textContent = scoreEnCours;
+    } else {
         joueurSuivant()
+        }
     }
-
 });
+
+//4
 let btnCommutez = document.querySelector('.btn-commuter');
     btnCommutez.addEventListener('click',()=> {
+        if(jouer) {
+            scoresGlobals[joueurActif - 1] += scoreEnCours;
+            document.getElementById(`score-${joueurActif}`).textContent = scoresGlobals[joueurActif - 1];
 
-    document.getElementById(`score-${joueurActif}`).textContent = scoresGlobals[joueurActif-1]+= scoreEnCours ;
-    joueurSuivant()
+            if (scoresGlobals[joueurActif - 1] >= 100) {
+                document.getElementById(`nom-${joueurActif}`).innerHTML = "<span style='color:green'>Bravo !!!</span>";
+                document.querySelector('.de').style.display = 'none';
+                document.querySelector(`.joueur-${joueurActif}-panel`).classList.add('vainqueur');
+                document.querySelector(`.joueur-${joueurActif}-panel`).classList.remove('active');
+                jouer = false;
 
-});
+            } else {
+                joueurSuivant();
+            }
+        }
+
+        });
+    //5
     function joueurSuivant(){
         joueurActif === 1? joueurActif=2 : joueurActif=1;
         scoreEnCours = 0;
         document.getElementById(`encours-${joueurActif}`).textContent = '0';
         document.querySelector('.joueur-1-panel').classList.toggle('active');
         document.querySelector('.joueur-2-panel').classList.toggle('active');
-
     };
 
-    ;
-        function initialiseLejeu (){
-        scoresGlobals = [0, 0];
-        scoreEnCours = 0;
-        joueurActif = 1;
-        document.getElementById('score-1').textContent = '0';
-        document.getElementById('score-2').textContent = '0';
-        document.getElementById('encours-1').textContent ='0';
-        document.getElementById('encours-2').textContent ='0';
-    }
+
+    //6
     let btnNeauveauJeu = document.querySelector('.btn-nouveau');
         btnNeauveauJeu.addEventListener('click',()=>{
             initialiseLejeu()
-})
+});
 
+    //7
+function initialiseLejeu (){
+    scoresGlobals = [0,0];
+    scoreEnCours = 0;
+    joueurActif = 1;
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('score-2').textContent = '0';
+    document.getElementById('encours-1').textContent = '0';
+    document.getElementById('encours-2').textContent ='0';
+    document.getElementById('nom-1').textContent = 'Joueur 1';
+    document.getElementById('nom-2').textContent ='Joueur 2';
+    document.querySelector('.joueur-1-panel').classList.remove('vainqueur');
+    document.querySelector('.joueur-2-panel').classList.remove('vainqueur');
+    document.querySelector('.joueur-2-panel').classList.remove('active');
+    document.querySelector('.joueur-1-panel').classList.add('active');
+
+};
